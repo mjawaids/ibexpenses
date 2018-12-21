088aa3616ae9e7a978603cbcda55739b5c9769fb
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2018-12-21 15:06:23 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -30,7 +30,8 @@ class TransactionController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -41,7 +42,8 @@ class TransactionController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Transaction  $transaction
+     * @param \App\Transaction $transaction
+     *
      * @return \Illuminate\Http\Response
      */
     public function show(Transaction $transaction)
@@ -52,7 +54,8 @@ class TransactionController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Transaction  $transaction
+     * @param \App\Transaction $transaction
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit(Transaction $transaction)
@@ -63,8 +66,9 @@ class TransactionController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Transaction  $transaction
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Transaction         $transaction
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Transaction $transaction)
@@ -75,7 +79,8 @@ class TransactionController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Transaction  $transaction
+     * @param \App\Transaction $transaction
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy(Transaction $transaction)
